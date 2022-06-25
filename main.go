03aaeaaa490a1d5218becf9d9f09c6bc10c4ba06
Aakash-Pandit <aakashpandit366@gmail.com>
2022-06-25 13:32:17 +0530

    setup config

--- main.go
+++ main.go
@@ -2,14 +2,17 @@ package main
 
 import (
 	"github.com/Aakash-Pandit/Blog-Post-Go-Fiber/config"
+	"github.com/Aakash-Pandit/Blog-Post-Go-Fiber/database"
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
 	app := fiber.New()
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello world")
-	})
-	config.ReadEnv()
-	app.Listen(":8080")
+	setupConfig := config.GetApplicationConfig()
+	database.InitializeDB(setupConfig)
+	db := database.GetDatabase()
+
+	config.SetupRoutes(app, db)
+
+	app.Listen(":" + setupConfig.BackendPort)
 }
