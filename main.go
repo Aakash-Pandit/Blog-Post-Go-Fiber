59300991b45e3d03e87987e8500ba8fe359eb6b0
Aakash-Pandit <aakashpandit366@gmail.com>
2022-06-25 06:53:00 +0530

    try to read env

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/Aakash-Pandit/Blog-Post-Go-Fiber/config"
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
 	app := fiber.New()
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello world")
 	})
+	config.ReadEnv()
 	app.Listen(":8080")
 }
