c79fdb074f75539dcb50bf33cb4ff575dde2809a
Aakash-Pandit <aakashpandit366@gmail.com>
2022-08-23 11:54:48 +0530

    updates name as primaryKey

--- models/users.go
+++ models/users.go
@@ -12,7 +12,7 @@ type AuthToken struct {
 }
 
 type User struct {
-	ID        uuid.UUID `json:"id" gorm:"primary key"`
+	ID        uuid.UUID `json:"id" gorm:"primaryKey"`
 	FirstName string    `json:"first_name" validate:"required"`
 	LastName  string    `json:"last_name" validate:"required"`
 	Email     string    `json:"email" validate:"required"`
