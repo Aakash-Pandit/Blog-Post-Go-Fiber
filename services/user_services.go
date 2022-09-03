631ca2772fb3bd3f0383dd4144f15a87f1e76745
Aakash-Pandit <aakashpandit366@gmail.com>
2022-09-03 10:59:59 +0530

    adds patch call for user

--- services/user_services.go
+++ services/user_services.go
@@ -1,6 +1,8 @@
 package services
 
 import (
+	"fmt"
+
 	"github.com/Aakash-Pandit/Blog-Post-Go-Fiber/auth"
 	"github.com/Aakash-Pandit/Blog-Post-Go-Fiber/models"
 	"github.com/Aakash-Pandit/Blog-Post-Go-Fiber/storage"
@@ -79,6 +81,62 @@ func CreateUser(context *fiber.Ctx) error {
 		}
 
 		return context.Status(fiber.StatusCreated).JSON(new_user)
+	}
+
+	return context.Status(fiber.StatusOK).JSON(user)
+}
+
+func UpdateUser(context *fiber.Ctx) error {
+	id := context.Params("id")
+	if id == "" {
+		context.Status(fiber.StatusInternalServerError).JSON(&fiber.Map{
+			"message": "id cannot be empty",
+		})
+		return nil
+	}
+
+	username := models.Username{}
+
+	err := context.BodyParser(&username)
+	if err != nil {
+		return context.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"detail": err.Error(),
+		})
+	}
+
+	errors := validators.ValidateUsernameStruct(username)
+
+	if errors != nil {
+		return context.Status(fiber.StatusBadRequest).JSON(errors)
+	}
+
+	db := storage.GetDatabase()
+	user := &models.User{}
+	err = db.Where("username = ?", username.Username).First(user).Error
+	if err == nil {
+		fmt.Println("err: ", err)
+		return context.Status(fiber.StatusBadRequest).JSON(&fiber.Map{
+			"detail": "Username already exists.",
+		})
+	}
+
+	user, err = FetchUserFromRequest(context)
+	if err != nil {
+		return context.Status(fiber.StatusUnauthorized).JSON(err)
+	}
+
+	if id != user.ID.String() {
+		return context.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"detail": "you are not authorized",
+		})
+	}
+
+	user.Username = username.Username
+	err = db.Where("id = ?", id).Updates(user).Error
+	if err != nil {
+		return context.Status(fiber.StatusBadRequest).JSON(&fiber.Map{
+			"detail": err.Error(),
+		})
 	}
 
 	return context.Status(fiber.StatusOK).JSON(user)
